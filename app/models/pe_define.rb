b82f115f8399d1b37b3a1004cfc157f6c42b252a
ben7th <ben7th@sina.com>
2016-07-12 19:16:22 +0800

    将 PeDefine 改成数据库模型

--- app/models/pe_define.rb
+++ app/models/pe_define.rb
@@ -1,27 +1,33 @@
 class PeDefine
-  CONFIG_DIR = Rails.root.join("config/pe_defines")
+  # CONFIG_DIR = Rails.root.join("config/pe_defines")
 
-  def self.find(name)
-    yaml_file_path = File.join(CONFIG_DIR, "#{name}.yml")
-    data = YAML.load_file(yaml_file_path)
-    self.new name, data
-  end
+  # def self.find(name)
+  #   yaml_file_path = File.join(CONFIG_DIR, "#{name}.yml")
+  #   data = YAML.load_file(yaml_file_path)
+  #   self.new name, data
+  # end
 
-  def self.all
-    names = %w{
-      三部九侯诊 经络诊 背诊 脉诊 脊柱诊 腹诊 舌诊 面诊
-    }
+  # def self.all
+  #   names = %w{
+  #     三部九侯诊 经络诊 背诊 脉诊 脊柱诊 腹诊 舌诊 面诊
+  #   }
 
-    names.map {|name|
-      yaml_file_path = File.join(CONFIG_DIR, "#{name}.yml")
-      data = YAML.load_file(yaml_file_path)
-      self.new name, data
-    }
-  end
+  #   names.map {|name|
+  #     yaml_file_path = File.join(CONFIG_DIR, "#{name}.yml")
+  #     data = YAML.load_file(yaml_file_path)
+  #     self.new name, data
+  #   }
+  # end
 
-  attr_reader :name, :data
-  def initialize(name, data)
-    @name = name
-    @data = data
-  end
+  # attr_reader :name, :data
+  # def initialize(name, data)
+  #   @name = name
+  #   @data = data
+  # end
+
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  belongs_to :fact_object
+  field :name # 体检项名称
 end
